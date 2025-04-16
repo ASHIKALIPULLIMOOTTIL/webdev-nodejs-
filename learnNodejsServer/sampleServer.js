d55c0956336cc37b21b010784d88d42012f556e9
UBUNTU <ashikaju01@gmail.com>
2025-04-16 15:15:50 +0530

    modified nodejs server

--- learnNodejsServer/sampleServer.js
+++ learnNodejsServer/sampleServer.js
@@ -1,12 +1,45 @@
-var http=require('http')
-var fs=require('fs')
-http.createServer(server).listen(7000)
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-function server(req,res){
-    fs.readFile('htmlserv.html',function(err,data){
-        res.writeHead(200,{'Content-Type':'text/html'})
-        res.write(data)
-        res.end()
-    })
-    
+http.createServer(server).listen(7000, function () {
+    console.log("Server running at http://localhost:7000/");
+});
+
+function server(req, res) {
+    var q = url.parse(req.url,true);
+     
+    if (q.pathname === '/') {
+        fs.readFile('htmlserv.html', function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading homepage');
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } 
+    else if (q.pathname === '/signup') {
+        fs.readFile('singup.html', function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading signup page');
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } 
+    else if (q.pathname === '/signupact') {
+        console.log(q.query)
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('hello'+'<h2>'+q.query.name+'</h2>');
+        res.end();
+    } 
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('404 Not Found');
+        res.end();
+    }
 }
