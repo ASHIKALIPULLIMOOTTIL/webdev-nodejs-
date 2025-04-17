1fca0bf59338e253cd52c49ab98d38c09d5f3cb1
UBUNTU <ashikaju01@gmail.com>
2025-04-17 12:27:16 +0530

    express handlebars

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const values=["cr7","lm10","njr"]
+  res.render('index', { values });
 });
 
 module.exports = router;
